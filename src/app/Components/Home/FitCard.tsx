15fd144fc74db50611acafb31d3db181b90a9dca
Md.Sumon <sumonahamed5566@gmail.com>
2026-09-25 21:53:14 +0600

    Data load from APi and added to the home page

---
+++ src/app/Components/Home/FitCard.tsx
@@ -0,0 +1,62 @@
+import { ExerciseType } from "@/app/Type/exerciseType";
+import Image from "next/image";
+import { AiFillFire } from "react-icons/ai";
+import { FaRegClock, FaRegStar } from "react-icons/fa";
+
+const FitCard = ({ exercise }: { exercise: ExerciseType }) => {
+  const {
+    image,
+    muscleGroups,
+    name,
+    equipment,
+    duration,
+    rating,
+    caloriesBurned,
+  } = exercise;
+
+  return (
+    <div className="bg-[#15171D] rounded-md ">
+      <Image
+        src={image}
+        alt={name}
+        width={0}
+        height={300}
+        sizes="100vw"
+        className="w-full h-auto"
+      />
+      <div className="p-6">
+        <ul className="flex items-center gap-2">
+          {muscleGroups.map((muscle) => (
+            <li
+              key={muscle}
+              className="bg-[#c2f800FF] text-xs font-bold text-black rounded-4xl px-[10px] py-1 "
+            >
+              {muscle}
+            </li>
+          ))}
+        </ul>
+        <h3 className="text-lg font-bold mt-3 mb-1 text-white">{name}</h3>
+        <p className="text-xs text-[#9ca3afFF]">{equipment}</p>
+
+        <div>
+          <ul className="flex gap-5 border border-[#20242eFF] rounded-sm mt-4 py-1 px-2">
+            <li className="flex items-center gap-1 text-[#9ca3afFF]">
+              <FaRegClock />
+              {duration}
+            </li>
+            <li className="flex items-center gap-1 text-[#9ca3afFF]">
+              <AiFillFire />
+              {caloriesBurned} kcal
+            </li>
+            <li className="flex items-center gap-1 text-[#9ca3afFF]">
+              <FaRegStar />
+              {rating}
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FitCard;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,38 @@
+import { Suspense } from "react";
+import FitCard from "./Components/Home/FitCard";
 import Hero from "./Components/Home/Hero";
+import { ExerciseType } from "./Type/exerciseType";
 
-export default function Home() {
+const exercise = async (): Promise<ExerciseType[]> => {
+  const res = await fetch("https://api.abcz.workers.dev/api/fitlog");
+
+  return res.json();
+};
+
+export default async function Home() {
+  const exerciseDatas = await exercise();
+
   return (
-    <section>
-      <Hero />
-    </section>
+    <>
+      <section>
+        <Hero />
+      </section>
+
+      <section className="container mx-auto mt-[64px] lg:px-0 px-4">
+        <div className="mb-[32px]">
+          <h2 className="text-3xl font-bold text-white mb-1">THE LIBRARY</h2>
+          <p className="text-sm text-[#9ca3afFF]">
+            Twelve lifts covering every major muscle group.
+          </p>
+        </div>
+        <div className="grid lg:grid-cols-3 gap-[32px]">
+          <Suspense fallback={"<p> Loading ..... </p>"}>
+            {exerciseDatas.map((exercise, index) => (
+              <FitCard key={index} exercise={exercise} />
+            ))}
+          </Suspense>
+        </div>
+      </section>
+    </>
   );
 }
